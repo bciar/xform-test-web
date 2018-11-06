3a13578a190cfd3bd0fec39069e72cdbda944020
DESKTOP-R24\asharp <bciar@yandex.com>
2018-11-06 05:40:02 +0100

    xform-test web 0.7

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, jsonify, request, redirect, send_file
+from flask import Flask, render_template, jsonify, request, redirect
 from static_methods import _run_background_process, _run_terminal_command
 from werkzeug.utils import secure_filename
 from flask import flash
@@ -15,15 +15,19 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/test')
-def test():
+@app.route('/xform_test/<string:filename>')
+def xform_test(filename):
     '''ret = _run_terminal_command('java -jar xform-test-0.3.0.jar temp_uploads\\MultipleTestCases.xml')
     print('return value', ret)'''
-    stdout, stderr = _run_background_process('java -jar xform-test-0.3.0.jar temp_uploads\\MultipleTestCases.xml')
-    error = stderr.decode("utf-8")
-    out = stdout.decode("utf-8")
-    # flash(out, "warning")
-    return render_template('index.html', **locals())
+    try:
+        stdout, stderr = _run_background_process('java -jar xform-test-0.3.0.jar temp_uploads\\'+filename)
+        error = stderr.decode("utf-8")
+        out = stdout.decode("utf-8")
+        # flash(out, "warning")
+        return render_template('index.html', **locals())
+    except Exception as err:
+        error = err.stderr.decode("utf-8")
+        return render_template('index.html', **locals())
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -42,7 +46,7 @@ def upload():
             os.mkdir(upload_folder)
             file.save(file_path)
 
-        return jsonify({'success': True})
+        return jsonify({'success': True, 'filename': filename})
     except Exception as err:
         msg = 'An unexpected error occurred:\n\n' + str(err)
         return jsonify({'success': False, 'message': msg})
