b32469380bbaa262555fed0512f39856fb208b19
joeflack4 <joeflack4@gmail.com>
2018-11-10 03:42:01 +0200

    Fixed an error where Heroku was throwing an error when it should not, causing our stderr/stdout process handling to mis-fire.
    Even if there is no error, Heroku always prints this message to the console
    under stderr. I file a bug report. - Joe 2018/11/10
    heroku_err_every_time = 'Picked up JAVA_TOOL_OPTIONS: -Xmx300m -Xss512k ' \
                            '-XX:CICompilerCount=2 -Dfile.encoding=UTF-8'

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ http://xform-test-docs.pma2020.org
 from glob import glob
 import platform
 import os
+import sys
 
 from flask import Flask, render_template, jsonify, request
 # noinspection PyProtectedMember
@@ -18,6 +19,12 @@ is_windows = platform.system() == 'Windows'
 path_char = '\\' if is_windows else '/'
 TEMP_DIR = 'temp'
 XFORM_TEST_EXECUTABLE = glob('bin/xform-test/*.jar')[0]
+LOGGING_ON = os.getenv('FLASK_ENV') in ('development', 'staging') or \
+             os.getenv('APP_SETTINGS') in ('development', 'staging')
+# Even if there is no error, Heroku always prints this message to the console
+# under stderr. I file a bug report. - Joe 2018/11/10
+heroku_err_every_time = 'Picked up JAVA_TOOL_OPTIONS: -Xmx300m -Xss512k ' \
+                        '-XX:CICompilerCount=2 -Dfile.encoding=UTF-8'
 
 
 @app.route('/')
@@ -42,8 +49,14 @@ def xform_test(filename):
                       TEMP_DIR + path_char + xml
             stdout, stderr = _run_background_process(command) if not \
                 is_windows else _run_windows_process(command)
+            stderr = '' if stderr == heroku_err_every_time else stderr
             if stderr:
-                return render_template('index.html', error=stderr)
+                stderr = stderr.replace(heroku_err_every_time, '')
+                if LOGGING_ON:
+                    print(stderr, file=sys.stderr)
+                    print(stdout)
+                return render_template('index.html', error=stderr,
+                                       stdout=stdout if LOGGING_ON else '')
         else:
             xml = filename
 
@@ -51,17 +64,25 @@ def xform_test(filename):
                   + TEMP_DIR + path_char + xml
         stdout, stderr = _run_background_process(command) if not \
             is_windows else _run_windows_process(command)
+        stderr = '' if stderr == heroku_err_every_time else stderr
         for file in glob('temp/*'):
             os.remove(file)
         if stderr:
+            stderr = stderr.replace(heroku_err_every_time, '')
+            if LOGGING_ON:
+                print(stderr, file=sys.stderr)
+                print(stdout)
             # TODO: @Joe: Fix so CLI does not return this in err msg.
             stderr = stderr.replace(
                 'Exception in thread "main" org.pma2020.xform_test.', '')
-            return render_template('index.html', error=stderr)
+            return render_template('index.html', error=stderr,
+                                   stdout=stdout if LOGGING_ON else '')
         else:
-            return render_template('index.html', out=stdout)
+            return render_template('index.html', out=stdout,
+                                   error=stderr if LOGGING_ON else '')
     except Exception as err:
-        return render_template('index.html', error=err)
+        print(str(err), file=sys.stderr)
+        return render_template('index.html', error=str(err))
 
 
 @app.route('/upload', methods=['POST'])
