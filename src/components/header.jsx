bf29c3e4a8823e1b0d31a889c265ebbdd39808e8
joeflack4 <joeflack4@gmail.com>
2018-11-20 14:35:03 -0500

    Initial commit for decoupled front-end version of xform-test-web.

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-center" style={{textAlign: 'center'}}>
+          XFormTest
+        </h1>
+      </div>
+    )
+  }
+}
